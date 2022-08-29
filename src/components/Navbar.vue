247c496184eeea5e30082e200b13d0c4cbe8ad47
Tabata Ruiz <tabata.ruiz@rhsoftware.com.br>
2022-08-29 16:15:24 -0300

    Implementando o Vue Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <nav>
+      <router-link to="/">
+        <!-- <img src="" alt="" /> -->
+      </router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped></style>
